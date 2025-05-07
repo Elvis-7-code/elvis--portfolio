0a08184a979771364d7d53f7c3d1816204dde0b1
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-07 14:33:54 +0300

    Hero.jsx changes

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero' className='hero'>
+        <h2>Hello! I'm Elvis</h2>
+        <p>A passionate Frontend Developer based in Kenya 🇰🇪</p>
+        <a href="#contact" className='btn'>Hire me</a>
+    </section>
+  );
+}
+
+export default Hero
